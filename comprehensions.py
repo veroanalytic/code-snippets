946662ca5cb3e5d6a75b3bead629e4e6f7ae6bcd
veroanalytic <veroanalytic@gmail.com>
2022-09-07 22:17:12 -0400

    comprehensions and .gitignore file added

---
+++ comprehensions.py
@@ -0,0 +1,34 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# https://medium.com/geekculture/10-python-mistakes-that-tell-youre-a-nooby-359487f22c97
+# 7. Never using comprehensions (or using them all the time)
+# ----------------------------------------------------------------------------------------------------------------------
+
+# countries = ['USA', 'UK', 'Canada']
+#
+# # Bad
+# lower_case = []
+# for country in countries:
+#     lower_case.append(country.lower())
+# print(country)
+#
+# # Good
+#
+# lower_case = [country.lower() for country in countries]
+# print(lower_case)
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Continue review here: 9/7/2022 22:13
+# https://medium.com/geekculture/10-python-mistakes-that-tell-youre-a-nooby-359487f22c97
+# 8. Using range(len())
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+# ----------------------------------------------------------------------------------------------------------------------
